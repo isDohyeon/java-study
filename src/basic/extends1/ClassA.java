6e935634558a3b5c057492e15f2dfa0792b772ee
dohyeon <head0412@naver.com>
2024-07-14 18:37:27 +0900

    2024 7/14 study 1

---
+++ src/basic/extends1/ClassA.java
@@ -0,0 +1,8 @@
+package basic.extends1;
+
+public class ClassA {
+
+    public ClassA() {
+        System.out.println("ClassA 생성자");
+    }
+}
---
+++ src/basic/poly/Parent.java
@@ -0,0 +1,10 @@
+package basic.poly;
+
+public class Parent {
+
+    public String value = "parent";
+
+    public void method() {
+        System.out.println("Parent method");
+    }
+}
